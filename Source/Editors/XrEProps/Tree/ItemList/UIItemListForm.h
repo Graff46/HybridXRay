7dd335e7df9e64541f11a4fcaebe0b809e233bf2
Romann <kriworuck.production@yandex.ru>
2023-03-18 00:05:26 +0300

    * [clang-format] Формат кода в файлах  part.4. [Editors]
    - Закончил "прилизование" кода, а то сил нет ломать глаза об кодстайл Панды.
      Не всё и не везде отфарматировал.
    
    - Походу дела заодно выявилось несколько косяков/опечаток в инклудах, так что даже полезно было.

--- Source/Editors/XrEProps/Tree/ItemList/UIItemListForm.h
+++ Source/Editors/XrEProps/Tree/ItemList/UIItemListForm.h
@@ -1,42 +1,72 @@
-#pragma once
-class XREPROPS_API UIItemListForm :public XrUI
+﻿#pragma once
+class XREPROPS_API UIItemListForm: public XrUI
 {
-	friend class UIItemListFormItem;
-	TOnILItemsFocused	OnItemsFocusedEvent;
-	TOnILItemFocused	OnItemFocusedEvent;
-	TOnItemRemove	OnItemRemoveEvent;
-	TOnItemRename	OnItemRenameEvent;
-	TOnItemCreate	OnItemCreateEvent;
-	TOnItemClone	OnItemCloneEvent;
-public:
-	UIItemListForm();
-	virtual ~UIItemListForm();
-public:
-	virtual void Draw();
-	void ClearList();
-	void ClearSelected();
-	void SelectItem(const char* name);
-	void AssignItems(ListItemsVec& items, const char* name_selection = nullptr, bool clear_floder = true, bool save_selected = false);
-	IC const ListItemsVec& GetItems()const {return m_Items;}
-	bool GetSelected(RStringVec& items)const;
-	int GetSelected(LPCSTR pref, ListItemsVec& items, bool bOnlyObject);
-public:
-	IC void		SetOnItemsFocusedEvent(TOnILItemsFocused e) { OnItemsFocusedEvent = e; }
-	IC void		SetOnItemFocusedEvent(TOnILItemFocused e) { OnItemFocusedEvent = e; }
-	IC void		SetOnItemRemoveEvent(TOnItemRemove e) { OnItemRemoveEvent = e; }
-	IC void		SetOnItemRenameEvent(TOnItemRename e) { OnItemRenameEvent = e; }
-	IC void		SetOnItemCreaetEvent(TOnItemCreate e) { OnItemCreateEvent = e; }
-	IC void		SetOnItemCloneEvent(TOnItemClone e) { OnItemCloneEvent = e; }
-public:
-	enum {
-		fMenuEdit =			(1<<0),
-		fMultiSelect =		(1<<1),
-	};
-	Flags32 m_Flags;
-	ListItemsVec m_Items;
-private:
-	void UpdateSelected(UIItemListFormItem* NewSelected);
-	UIItemListFormItem m_RootItem;
-	UIItemListFormItem* m_SelectedItem;
-};
+    friend class UIItemListFormItem;
+    TOnILItemsFocused OnItemsFocusedEvent;
+    TOnILItemFocused  OnItemFocusedEvent;
+    TOnItemRemove     OnItemRemoveEvent;
+    TOnItemRename     OnItemRenameEvent;
+    TOnItemCreate     OnItemCreateEvent;
+    TOnItemClone      OnItemCloneEvent;
 
+public:
+    UIItemListForm();
+    virtual ~UIItemListForm();
+
+public:
+    virtual void Draw();
+    void         ClearList();
+    void         ClearSelected();
+    void         SelectItem(const char* name);
+    void         AssignItems(
+                ListItemsVec& items,
+                const char*   name_selection = nullptr,
+                bool          clear_floder   = true,
+                bool          save_selected  = false);
+    IC const ListItemsVec& GetItems() const
+    {
+        return m_Items;
+    }
+    bool GetSelected(RStringVec& items) const;
+    int  GetSelected(LPCSTR pref, ListItemsVec& items, bool bOnlyObject);
+
+public:
+    IC void SetOnItemsFocusedEvent(TOnILItemsFocused e)
+    {
+        OnItemsFocusedEvent = e;
+    }
+    IC void SetOnItemFocusedEvent(TOnILItemFocused e)
+    {
+        OnItemFocusedEvent = e;
+    }
+    IC void SetOnItemRemoveEvent(TOnItemRemove e)
+    {
+        OnItemRemoveEvent = e;
+    }
+    IC void SetOnItemRenameEvent(TOnItemRename e)
+    {
+        OnItemRenameEvent = e;
+    }
+    IC void SetOnItemCreaetEvent(TOnItemCreate e)
+    {
+        OnItemCreateEvent = e;
+    }
+    IC void SetOnItemCloneEvent(TOnItemClone e)
+    {
+        OnItemCloneEvent = e;
+    }
+
+public:
+    enum
+    {
+        fMenuEdit    = (1 << 0),
+        fMultiSelect = (1 << 1),
+    };
+    Flags32      m_Flags;
+    ListItemsVec m_Items;
+
+private:
+    void                UpdateSelected(UIItemListFormItem* NewSelected);
+    UIItemListFormItem  m_RootItem;
+    UIItemListFormItem* m_SelectedItem;
+};
